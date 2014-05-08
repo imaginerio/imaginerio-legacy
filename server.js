6b8d34a3377c8aca38125f7a92fc31f4718f2f04
David Heyman <dave@axismaps.com>
2014-05-08 14:53:41 +0100

    Added CORS to server.js

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ var express = require( 'express' ),
 
 var app = express();
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader( 'Access-Control-Allow-Origin', 'http://localhost' );
+
+    // Request methods you wish to allow
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get( '/timeline', meta.timeline );
 app.get( '/layers/:year', meta.layers );
 app.get( '/names/:year/:word', meta.names );
