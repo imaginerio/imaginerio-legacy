074606eae4b09deeb1a05994bb56222e833d7463
David Heyman <dave@axismaps.com>
2014-05-08 09:48:10 +0100

    Revert mapnik tests

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.get( '/timeline', meta.timeline );
 app.get( '/layers/:year', meta.layers );
 app.get( '/names/:year/:word', meta.names );
-app.get( '/mapnik', geo.mapnik_test );
+//app.get( '/countries/:id/polygon/:srid', geo.polygonSrid);
 
 app.listen( 3000 );
 console.log( 'Listening on port 3000...' );
