2b7437e4d8ce0271cd372c6acfdb7045b85cc4d1
David Heyman <dave@axismaps.com>
2014-05-08 09:46:39 +0100

    Revert heroku

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 var express = require( 'express' ),
-	logfmt = require( 'logfmt' ),
     meta = require( './server/meta' ),
     geo = require( './server/geo' );
 
 var app = express();
-app.use( logfmt.requestLogger() );
 
 app.get( '/timeline', meta.timeline );
 app.get( '/layers/:year', meta.layers );
 app.get( '/names/:year/:word', meta.names );
 app.get( '/mapnik', geo.mapnik_test );
 
-var port = Number( process.env.PORT || 5000 );
-app.listen( port, function()
-{
-  console.log( "Listening on " + port );
-});
+app.listen( 3000 );
+console.log( 'Listening on port 3000...' );
