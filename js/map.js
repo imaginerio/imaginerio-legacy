760b3ce9724d975f8a3592d17fab9dc79722be59
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-06-14 14:44:45 -0500

    Convert device orientation direction to compass heading
    Taken from w3c site directly.

--- js/map.js
+++ js/map.js
@@ -108,10 +108,17 @@ function init_map()
 
 		// Check to make sure the browser supprots DeviceOrientationEvents
 		if (window.DeviceOrientationEvent) {
-			console.log('supported');
 		  window.addEventListener('deviceorientation', function(event) {
 				$( ".you-are-here-icon i" ).show();
-				yahIcon.setRotationAngle(event.alpha);
+				if( event.webkitCompassHeading ) //for safari
+				{
+      		compassdir = event.webkitCompassHeading;
+    		}
+				else
+				{
+    			compassdir = compassHeading(event.alpha, event.beta, event.gamma);
+    		}
+				yahIcon.setRotationAngle(compassdir);
 		  });
 		} else {
 			$( ".you-are-here-icon i" ).hide();
@@ -323,4 +330,36 @@ function export_map()
 	var url = server + "/export/" + lang + "/" + year + "/" + layerstring + "/" + raster + "/" + map.getBounds().toBBoxString() + "/";
 	document.getElementById( 'download_iframe' ).src = url;
 	window.setTimeout( function(){ $( "#export" ).removeClass( "loading" ); }, 2000 );
+}
+
+function compassHeading( alpha, beta, gamma )
+{
+	var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+  var _x = beta  ? beta  * degtorad : 0; // beta value
+  var _y = gamma ? gamma * degtorad : 0; // gamma value
+  var _z = alpha ? alpha * degtorad : 0; // alpha value
+
+  var cX = Math.cos( _x );
+  var cY = Math.cos( _y );
+  var cZ = Math.cos( _z );
+  var sX = Math.sin( _x );
+  var sY = Math.sin( _y );
+  var sZ = Math.sin( _z );
+
+  // Calculate Vx and Vy components
+  var Vx = - cZ * sY - sZ * sX * cY;
+  var Vy = - sZ * sY + cZ * sX * cY;
+
+  // Calculate compass heading
+  var compassHeading = Math.atan( Vx / Vy );
+
+  // Convert compass heading to use whole unit circle
+  if( Vy < 0 ) {
+    compassHeading += Math.PI;
+  } else if( Vx < 0 ) {
+    compassHeading += 2 * Math.PI;
+  }
+
+  return compassHeading * ( 180 / Math.PI ); // Compass Heading (in degrees)
 }
