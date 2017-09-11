2f89b8189ab7079c3afe3389aaa6952c66ed13ef
David Heyman <dave@axismaps.com>
2017-09-11 15:22:06 +0100

    Removed server files

--- server/dev.js
+++
@@ -1,7 +0,0 @@
-exports.checkQuery = function( query, req ){
-	if( req.headers.host.match( /-dev/ ) ){
-		query = query.replace( /(FROM (?=[a-z])|INNER JOIN (?=[a-z]))([a-z]*)/g, '\$1\$2_dev' );
-	}
-	
-	return query;
-}
--- server/mapnik/batch_raster.sh
+++
@@ -1,5 +0,0 @@
-for f in SSID*
-do
-   gdal_translate $f ../$f  -ot Byte -co PHOTOMETRIC=RGB -co TILED=YES -co COMPRESS=LZW -co BLOCKXSIZE=128 -co BLOCKYSIZE=128
-done
-
